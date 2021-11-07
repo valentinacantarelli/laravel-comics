88bd334641bc9aad1f1ae1d81fce1393bb69178f
valentina cantarelli <valentinacanterina@gmail.com>
2021-11-07 17:35:06 +0100

    aggiunto header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,22 @@
+<section class="header">
+    <div class="topHeader">
+        <div class="bottomHeader">
+            <img src="{{asset("images/dc-logo.png")}}">
+            <ul>
+                <li><a href="">CHARACTERS</a></li>
+                <li><a href="">COMICS</a></li>
+                <li><a href="">MOVIES</a></li>
+                <li><a href="">TV</a></li>
+                <li><a href="">GAMES</a></li>
+                <li><a href="">COLLECTIBLES</a></li>
+                <li><a href="">VIDEOS</a></li>
+                <li><a href="">FANS</a></li>
+                <li><a href="">NEWS</a></li>
+                <li><a href="">SHOP</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="hero">
+        <img src="{{asset("images/jumbotron.jpg")}}" alt="">
+    </div>
+</section>
